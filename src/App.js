bc02abfc2147f59ea356c82b10c9445a056ccbe1
Jonathan Rod <jonyrm91@gmail.com>
2022-01-16 17:20:50 +0100

    feat: we define the states and the basis for the structure of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Values from 'values.js';
+import { SingleColor } from './SingleColor';
 
 export const App = () => {
+  const [color, setColor] = useState('');
+  const [error, SerError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  };
+
   return (
-    <div>
-      <p>Color Generator App</p>
-    </div>
+    <>
+      <section className="container">
+          
+      </section>
+      <section className="colors">
+          <h4>list goes here</h4>
+      </section>
+    </>
   );
 };
---
+++ src/SingleColor.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { rgbToHex } from './utils';
+
+export const SingleColor = () => {
+  return <h4>Single Color</h4>;
+};

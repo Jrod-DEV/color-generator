a8df7f1f9ae83473868ae7b145d635cba6e073a0
Jonathan Rod <jonyrm91@gmail.com>
2022-01-16 18:04:36 +0100

    feat: submit btn and color state

--- src/App.js
+++ src/App.js
@@ -16,10 +16,21 @@ export const App = () => {
   return (
     <>
       <section className="container">
-          
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
       </section>
       <section className="colors">
-          <h4>list goes here</h4>
+        <h4>list goes here</h4>
       </section>
     </>
   );
